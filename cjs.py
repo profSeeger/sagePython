68b9d7689ac0b8aa5ea15e472a26062b0366a851
Christopher J. Seeger <cjseeger@users.noreply.github.com>
2023-01-10 14:07:37 -0600

    updated to a list

--- cjs.py
+++ cjs.py
@@ -1,1 +1,9 @@
-#Chris's Python
+# Chris's Python
+KAS = "Nottingham Forest"
+SRS = "Tottenham Hotspur"
+CJS = "Brighton & Hove Albion"
+JLS = "Manchester City"
+
+print(KAS, SRS, CJS, JLS)
+print("-------")
+print(KAS, SRS, CJS, JLS, sep=", ")
